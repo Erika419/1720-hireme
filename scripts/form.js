6d1923d8e215df54f3dac36cae6deda6550bedb6
Erika Costecalde <11122171@uvu.edu>
2026-09-04 14:49:14 -0600

    deleted testing + added 'first' to form.js

--- scripts/form.js
+++ scripts/form.js
@@ -1,5 +1,4 @@
-const myInfo1= new URLSearchParams(window.location);
-console.log(myInfo1);
-
 const myInfo= new URLSearchParams(window.location.search);
 console.log(myInfo);
+
+console.log(myInfo.get('first'));
