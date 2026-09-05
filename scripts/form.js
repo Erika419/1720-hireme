0dc58b5b2d8db12fc0e9fda98f1086892121df13
Erika Costecalde <11122171@uvu.edu>
2026-09-05 14:17:15 -0600

    finished form.js

--- scripts/form.js
+++ scripts/form.js
@@ -2,4 +2,9 @@ const myInfo= new URLSearchParams(window.location.search);
 console.log(myInfo);
 
 document.querySelector("#results").innerHTML=`
-<p> Application From: ${myInfo.get('first')} ${myInfo.get('last')}</p>` 
+<p> <span> Application From: </span>${myInfo.get('first')} ${myInfo.get('last')}</p> 
+<p> <span> Applying As: </span>${myInfo.get('who')}</p> 
+<p> <span> Your Phone: </span>${myInfo.get('telephone')}</p> 
+<p> <span> Your Email: </span>${myInfo.get('Email')}</p>
+<p> <span> Your Portfolio: </span>${myInfo.get('portfolio')}</p>
+<p> <span> Your Comments: </span>${myInfo.get('comments')}</p>`  
